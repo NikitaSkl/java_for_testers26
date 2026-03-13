7197cb9ef4b682191f0cd005f304289014916add
Nikita Skliar <nikitassklyar@gmail.com>
2026-03-13 13:37:02 +0300

    Group removal tests added

--- addressbook_web_tests26/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests26/src/test/java/tests/GroupCreationTests.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class GroupCreationTest {
+public class GroupCreationTests {
     private static WebDriver driver;
 
     @BeforeEach
