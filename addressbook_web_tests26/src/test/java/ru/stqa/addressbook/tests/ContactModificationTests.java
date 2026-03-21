29af0b2c4d149538fd1b21640cd237d41956121f
Nikita Skliar <nikitassklyar@gmail.com>
2026-03-21 21:44:25 +0300

    Fix incorrect index bug

--- addressbook_web_tests26/src/test/java/ru/stqa/addressbook/tests/ContactModificationTests.java
+++ addressbook_web_tests26/src/test/java/ru/stqa/addressbook/tests/ContactModificationTests.java
@@ -13,13 +13,17 @@ public class ContactModificationTests extends TestBase {
         if (!app.contacts().isContactPresent()) {
             app.contacts().createContact(new Contact());
         }
-        var contactList=app.contacts().getList();
-        var index=new Random().nextInt(contactList.size()-1);
-        app.contacts().modifyContact(contactList.get(index),new Contact().withFirstName("modified firstName").withLastName("modified lastName"));
-        Comparator<Contact> compareById=(c1,c2)->Integer.parseInt(c1.id())-Integer.parseInt(c2.id());
-        contactList.set(index,contactList.get(index).withFirstName("modified firstName").withLastName("modified lastName"));
+        var contactList = app.contacts().getList();
+        int index;
+        if (app.contacts().getCount() == 1) index = 0;
+        else index = new Random().nextInt(contactList.size() - 1);
+        String newFirstName = randomString(), newLastname = randomString();
+        app.contacts().modifyContact(contactList.get(index), new Contact().withFirstName(newFirstName).withLastName(newLastname));
+        Comparator<Contact> compareById = (c1, c2) -> Integer.parseInt(c1.id()) - Integer.parseInt(c2.id());
+        contactList.set(index, contactList.get(index).withFirstName(newFirstName).withLastName(newLastname));
         contactList.sort(compareById);
-        var actualContactList=app.contacts().getList();
-        Assertions.assertEquals(contactList,actualContactList);
+        var actualContactList = app.contacts().getList();
+        actualContactList.sort(compareById);
+        Assertions.assertEquals(contactList, actualContactList);
     }
 }
